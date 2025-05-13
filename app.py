8b6a0588c8767e44537d9fd1c490593e9f651f8e
임재민 <min031020@naver.com>
2025-05-13 15:20:57 +0900

    new

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ st.title('오픈소스 소프트웨어 과제')
 
 options=["부산", "서울", "대전", "광주"]
 
-answer1=st.radio("다음 중 대한민국의 수도는 어디인가요??",options)
+answer1=st.radio("(객관식) 다음 중 대한민국의 수도는 어디인가요?",options)
 
-answer2=st.text_input("데이터 애플리케이션을 빠르게 구축할 수 있는 파이썬 기반 오픈 소스 프레임워크는?")
+answer2=st.text_input("(주관식) 데이터 애플리케이션을 빠르게 구축할 수 있는 파이썬 기반 오픈 소스 프레임워크는?")
 
 if st.button("제출"):
     if answer1 == "서울":
